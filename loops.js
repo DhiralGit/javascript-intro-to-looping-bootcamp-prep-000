b7cacbb7143d77aed1111aa6a9db9916d1f50cc7
Dhiral Patel <dhiralpatel794@gmail.com>
2018-05-29 18:22:36 +0000

    automatically backed up by learn

--- loops.js
+++ loops.js
@@ -13,5 +13,12 @@ return 'done';
 }
 
 function maybeTrue(){
-  return Math.random() >= 0.50;
+  return Math.random() >= 0.5;
+}
+
+function doWhileLoop(array){
+  do{
+    array.pop();
+  }while(array.length > 0 && maybeTrue());
+  return array;
 }
